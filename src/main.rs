5e58dd3e78f85e7bab45fcb0e6df0087a8b77314
nori <norifxsc@gmail.com>
2022-07-17 13:09:03 +0900

    display ok

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,43 @@ fn main() {
     let mut word = String::new();
     io::stdin().read_line(&mut word).ok();
     let words = word.trim().to_string();
-    println!("{}", words);
-    let mut mem: [u32; 500] = [0; 500];
+    let chars_vec: Vec<char> = words.chars().collect();
+    let mut mem: [usize; 500] = [0; 500];
     let mut pointer: usize = 0;
-    let mut l_mem: [u32; 100] = [0; 100];
-    let mut l_pointer: u32 = 0;
-    let mut skipcount: u32 = 0;
-    for symbol in words.chars() {
-        match symbol {
+    let mut l_mem: [usize; 100] = [0; 100];
+    let mut l_pointer: usize = 0;
+    let mut skipcount: usize = 0;
+    let mut i: usize = 0;
+    loop {
+        if i >= chars_vec.len() {
+            break;
+        }
+        match chars_vec[i] {
             '>' => pointer += 1,
             '<' => pointer -= 1,
             '+' => mem[pointer] += 1,
             '-' => mem[pointer] -= 1,
-            '.' => match char::from_u32(mem[pointer]) {
+            '.' => match char::from_u32(mem[pointer] as u32) {
                 Some(x) => print!("{}", x),
                 None => println!("None"),
             },
             ',' => todo!(),
             '[' => {
-                // l_mem[l_mem]
+                l_mem[l_pointer] = i;
+                l_pointer += 1;
             }
-            ']' => todo!(),
-            _ => {}
+            ']' => {
+                if mem[pointer] == 0 {
+                    l_pointer -= 1;
+                } else {
+                    i = l_mem[l_pointer - 1];
+                }
+            }
+            _ => {
+                panic!("excep char")
+            }
         };
+        i += 1;
     }
     println!("{:?}", mem);
 }
