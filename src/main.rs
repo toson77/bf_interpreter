835d006388186f0eb00e2b2d3ca4363f0fe692bc
nori <norifxsc@gmail.com>
2022-07-17 22:44:02 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,12 @@
 use std::char;
 use std::io;
+mod work;
+use work::interpreter;
 fn main() {
     let mut word = String::new();
     io::stdin().read_line(&mut word).ok();
     let words = word.trim().to_string();
     let chars_vec: Vec<char> = words.chars().collect();
-    let mut mem: [usize; 500] = [0; 500];
-    let mut pointer: usize = 0;
-    let mut l_mem: [usize; 100] = [0; 100];
-    let mut l_pointer: usize = 0;
-    let mut skipcount: usize = 0;
-    let mut i: usize = 0;
-    loop {
-        if i >= chars_vec.len() {
-            break;
-        }
-        if skipcount >= 1 {
-            match chars_vec[i] {
-                '[' => skipcount += 1,
-                ']' => skipcount -= 1,
-                _ => {}
-            }
-            i += 1;
-            continue;
-        }
-        match chars_vec[i] {
-            '>' => pointer += 1,
-            '<' => pointer -= 1,
-            '+' => mem[pointer] += 1,
-            '-' => mem[pointer] -= 1,
-            '.' => match char::from_u32(mem[pointer] as u32) {
-                Some(x) => print!("{}", x),
-                None => println!("None"),
-            },
-            ',' => todo!(),
-            '[' => {
-                if mem[pointer] == 0 {
-                    skipcount += 1;
-                } else {
-                    l_mem[l_pointer] = i;
-                    l_pointer += 1;
-                }
-            }
-            ']' => {
-                if mem[pointer] == 0 {
-                    l_pointer -= 1;
-                } else {
-                    i = l_mem[l_pointer - 1];
-                }
-            }
-            _ => {
-                panic!("excep char")
-            }
-        };
-        i += 1;
-    }
-    println!("{:?}", mem);
+    let result = interpreter(chars_vec);
+    println!("{}", result);
 }
---
+++ src/work.rs
@@ -0,0 +1,56 @@
+use std::char;
+pub fn interpreter(chars_vec: Vec<char>) -> String {
+    let mut result = String::new();
+    let mut mem: [usize; 500] = [0; 500];
+    let mut pointer: usize = 0;
+    let mut l_mem: [usize; 100] = [0; 100];
+    let mut l_pointer: usize = 0;
+    let mut skipcount: usize = 0;
+    let mut i: usize = 0;
+    loop {
+        if i >= chars_vec.len() {
+            break;
+        }
+        if skipcount >= 1 {
+            match chars_vec[i] {
+                '[' => skipcount += 1,
+                ']' => skipcount -= 1,
+                _ => {}
+            }
+            i += 1;
+            continue;
+        }
+        match chars_vec[i] {
+            '>' => pointer += 1,
+            '<' => pointer -= 1,
+            '+' => mem[pointer] += 1,
+            '-' => mem[pointer] -= 1,
+            '.' => match char::from_u32(mem[pointer] as u32) {
+                Some(x) => result.push(x),
+                None => println!("None"),
+            },
+            ',' => todo!(),
+            '[' => {
+                if mem[pointer] == 0 {
+                    skipcount += 1;
+                } else {
+                    l_mem[l_pointer] = i;
+                    l_pointer += 1;
+                }
+            }
+            ']' => {
+                if mem[pointer] == 0 {
+                    l_pointer -= 1;
+                } else {
+                    i = l_mem[l_pointer - 1];
+                }
+            }
+            _ => {
+                panic!("excep char")
+            }
+        };
+        i += 1;
+    }
+    println!("{:?}", mem);
+    result
+}
